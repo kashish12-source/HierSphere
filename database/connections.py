04779922c29972a56f842fd463de902beac27bef
Kashish <kashishvishnoi2006@gmail.com>
2026-05-10 02:43:45 +0530

    initial

---
+++ database/connections.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+DATABASE_URL="postgresql://postgres:root@localhost/HireSphere"
+
+engine=create_engine(DATABASE_URL)
+
+SessionLocal=sessionmaker(
+    autocommit=False,
+    autoflush=Fasle,
+    bind=engine
+
+)
+
+Base=declarative_base()
